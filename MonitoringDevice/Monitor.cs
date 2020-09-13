07c8b9a20185d8d9b8e8b6b924a8f819a9daaa47
aayushvirkar <51874905+aayushvirkar@users.noreply.github.com>
2020-09-13 19:34:32 +0530

    Updated Monitor

--- MonitoringDevice/Monitor.cs
+++ MonitoringDevice/Monitor.cs
@@ -4,20 +4,21 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Markup;
 
 namespace MonitoringDevice
 {
+    
     public class MonitoringDevice
     {
-        string[] buffer;
+        public static string[] buffer;
         string[] temp;
-
-       
-        public int ReadingData()
+      
+        public int ReadingData(string filePath)
         {
-            buffer = System.IO.File.ReadAllLines("C:/Users/Aayush/Desktop/test.csv");
+            buffer = System.IO.File.ReadAllLines(filePath);
             if (buffer.Length==0)
             {
                 Console.WriteLine("FILE IS EMPTY");
@@ -27,56 +28,77 @@ namespace MonitoringDevice
 
         }
 
-        
-        public int ProcessingData()
+        public int FormatChecker()
         {
-            
-            for(int i=1; i<buffer.Length; i++)
-            {
-                if (buffer[i].IndexOf(',') != 2)
-                {
-                    Console.WriteLine("File not in correct format");
-                    return 0;
-                }
-
-                temp = buffer[i].Split(',');
-                buffer[i] = temp[0] + " " + temp[1];
-            }
-            return 1;
-        }
-
-        public int SendingData()
-        {
-
-            int flag = 1;
             for(int i=1;i<buffer.Length;i++)
             {
-                
-                if ((buffer[i].Substring(0, 1)) == " ")
+               
+                if (!(buffer[i].Contains(',')))
                 {
-                    Console.WriteLine("Missing Temprature value");
-                    flag = 0;
+                    Console.WriteLine("Device Malfunction");
+                    return 0;
+                }
+            }
+            
+            return 1;
+        }
+        public int ProcessingData()
+        {
+            int flag = 0;
+            
+            for(int i=1; i<buffer.Length; i++)  
+            {   
+
+                temp = buffer[i].Split(',');
+
+                if (temp[0]=="")
+                {
+                    Console.WriteLine("Missing Temprature Data");
+                    flag = 1;  
+                    continue;
+                }
+                else if(temp[1]=="")
+                {
+                    Console.WriteLine("Missing Humidity Data");
+                    flag = 1;
                     continue;
                 }
 
-                else if ((buffer[i].Length) == 3)
-                {
-                    Console.WriteLine("Missing Humidity value");
-                    flag = 0;
-                    continue;
-                }
-                   
-                Console.WriteLine(buffer[i]);
+                buffer[i] = temp[0] + " " + temp[1];
+                
             }
             return flag;
         }
 
+        public void SendingData()
+        {
+            for(int i=1;i<buffer.Length;i++)
+            {
+                Thread.Sleep(3000);
+                if(!(buffer[i].Contains(',')))
+                Console.WriteLine(buffer[i]);
+            }
+            
+        }
+
+            
+
         static void Main(string[] args)
         {
             MonitoringDevice obj = new MonitoringDevice();
-            obj.ReadingData();
-            obj.ProcessingData();
-            obj.SendingData();
+
+            string path = "C:/Users/Aayush/Desktop/test.csv";
+
+            obj.ReadingData(path);
+
+            int x = obj.FormatChecker();
+
+            if (x == 1)
+            {
+                obj.ProcessingData();
+                obj.SendingData();
+               
+            }
         }
     }
 }
