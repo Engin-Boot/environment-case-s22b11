df6f9fceaf2d9cca8d0f85419bfd6dc7669d38fc
shweta-shinde <47037519+shweta-shinde@users.noreply.github.com>
2020-09-15 19:47:19 +0530

    added workflows

--- ConditionsAnalyzer/Reporter.cs
+++ ConditionsAnalyzer/Reporter.cs
@@ -12,6 +12,8 @@ namespace ConditionsAnalyzer
         public void SendMessage(string message)
         {
             Console.WriteLine($"SMS Reporter :  {message}");
+            LogFileLibrary.LogFile obj = new LogFileLibrary.LogFile("../../../logfile.txt", message);
+            obj.WriteToLogFile();
         }
     }
     public class EmailReporter : IReporter
@@ -19,6 +21,8 @@ namespace ConditionsAnalyzer
         public void SendMessage(string message)
         {
             Console.WriteLine($"Email Reporter :  {message}");
+            LogFileLibrary.LogFile obj = new LogFileLibrary.LogFile("../../../logfile.txt", message);
+            obj.WriteToLogFile();
         }
     }
 }
